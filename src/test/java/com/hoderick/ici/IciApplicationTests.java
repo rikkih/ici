ee1d02566a090142e5b30ba48967f50ec18dea49
Hoderick <rikkih@protonmail.com>
2025-07-08 11:20:00 +0100

    init

---
+++ src/test/java/com/hoderick/ici/IciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoderick.ici;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
